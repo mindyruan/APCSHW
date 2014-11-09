8211e7dbc121a194cf1532f271155406bb1a9c72
Mindy <mindyruan.763002@gmail.com>
2014-11-08 23:37:59 -0500

    tested the new functions

--- Driver.java
+++ Driver.java
@@ -8,10 +8,14 @@ public class Driver{
 	b.add(2);
 	b.add('a');
 	b.add("another string");
+	b.set(1,"it works!");
 	System.out.println(b.toString());
+	System.out.println(b.get(2));
 	b.resize(10);
 	System.out.println(b.toString());
 	b.resize(2);
+	System.out.println(b.toString());
+	b.clear();
 	System.out.println(b.toString());
     }
 
