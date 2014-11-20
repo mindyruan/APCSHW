e156386402e1a820b4341098bc7a3bd08335fc21
Mindy <mindyruan.763002@gmail.com>
2014-11-19 21:17:16 -0500

    03WordSearch

---
+++ WordSearch.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class WordSearch{
+
+    public static void main(String[]arg){
+	WordGrid g = new WordGrid(4,4);
+	System.out.println(g.toString());
+
+	//System.out.println(g.toString());
+	//char[][]lol = new char[2][2];
+	//lol[1][1] = 'l';
+        //System.out.println(""+lol[1][1]);
+	//System.out.println(lol.length);
+
+    }
+
+
+}
