8293e836d071062ac019ee03cd04a0819ea8817d
Mindy Ruan <mindyruan@users.noreply.github.com>
2014-11-23 14:53:26 -0500

    testing vertical add

--- 03WordSearch/WordSearch.java
+++ 03WordSearch/WordSearch.java
@@ -3,12 +3,18 @@ public class WordSearch{
     public static void main(String[]arg){
 	WordGrid g = new WordGrid(4,4);
 	System.out.println(g.toString());
+	/* HORIZONTAL
 	System.out.println(g.addWordHorizontal("cat", 0,0));
 	System.out.println(g.addWordHorizontal("cat", 1,2));
 	System.out.println(g.addWordHorizontal("wrecked", 2,1));
 	System.out.println(g.toString());
+	*/
 
+	// VERTICAL
+	System.out.println(g.addWordVertical("cat", 0,0));
+	System.out.println(g.addWordVertical("cat", 2,2));
+	System.out.println(g.addWordVertical("wrecked", 2,1));
+	System.out.println(g.toString());
     }
-
 
 }
