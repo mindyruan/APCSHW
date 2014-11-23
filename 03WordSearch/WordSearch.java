2e92fc891ed6cbae12feb7591734b57052129b08
Mindy Ruan <mindyruan@users.noreply.github.com>
2014-11-23 14:43:29 -0500

    putting stuff places

--- 03WordSearch/WordSearch.java
+++ 03WordSearch/WordSearch.java
@@ -3,9 +3,10 @@ public class WordSearch{
     public static void main(String[]arg){
 	WordGrid g = new WordGrid(4,4);
 	System.out.println(g.toString());
-	System.out.println(g.addWordHorizontal("cat", 0,1));
-	System.out.println(g.addWordHorizontal("cat", 0,2));
-	System.out.println(g.addWordHorizontal("wrecked", 0,1));
+	System.out.println(g.addWordHorizontal("cat", 0,0));
+	System.out.println(g.addWordHorizontal("cat", 1,2));
+	System.out.println(g.addWordHorizontal("wrecked", 2,1));
+	System.out.println(g.toString());
 
     }
 
