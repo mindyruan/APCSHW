44b844f4405265993115070af7d09162f3c3fc26
Mindy Ruan <mindyruan@users.noreply.github.com>
2014-11-23 14:24:55 -0500

    testing whether words fit and which way

--- 03WordSearch/WordSearch.java
+++ 03WordSearch/WordSearch.java
@@ -1,16 +1,11 @@
-import java.util.ArrayList;
-
 public class WordSearch{
 
     public static void main(String[]arg){
 	WordGrid g = new WordGrid(4,4);
 	System.out.println(g.toString());
-
-	//System.out.println(g.toString());
-	//char[][]lol = new char[2][2];
-	//lol[1][1] = 'l';
-        //System.out.println(""+lol[1][1]);
-	//System.out.println(lol.length);
+	System.out.println(g.addWordHorizontal("cat", 0,1));
+	System.out.println(g.addWordHorizontal("cat", 0,2));
+	System.out.println(g.addWordHorizontal("wrecked", 0,1));
 
     }
 
