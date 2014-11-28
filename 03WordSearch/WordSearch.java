3fc0b115614189447ceb5e4af2b476b457cd1323
Mindy Ruan <mindyruan@users.noreply.github.com>
2014-11-28 16:19:29 -0500

    to show or not to show answers

--- 03WordSearch/WordSearch.java
+++ 03WordSearch/WordSearch.java
@@ -7,16 +7,21 @@ public class WordSearch{
 	WordGrid g;
         if(arg.length == 2){
 	    System.out.println("2");
-	    g = new WordGrid(20,20,Integer.parseInt(arg[0]),Integer.parseInt(arg[1]));
+	    g = new WordGrid(15,30,Integer.parseInt(arg[0]));
+	    if (arg[1].equals("1")){
+		g.loadWordsFromFile("words.txt",true);
+	    }else{
+		g.loadWordsFromFile("words.txt",false);
+	    }
 	}else if(arg.length == 1){
 	    System.out.println("1");
-	    g = new WordGrid(20,20,Integer.parseInt(arg[0]),0);
+	    g = new WordGrid(15,30,Integer.parseInt(arg[0]));
+	    g.loadWordsFromFile("words.txt",false);
 	}else{
 	    System.out.println("default");
-	    g = new WordGrid(15,30,1,0);
+	    g = new WordGrid(15,30,0);
+	    g.loadWordsFromFile("words.txt",false);
 	}
-
-	g.loadWordsFromFile("words.txt",false);
     }
 
 }
