c8d78242c7fe725c284413ff310e44954545682c
Mindy Ruan <mindyruan@users.noreply.github.com>
2014-11-28 16:05:05 -0500

    loadWordsFromFile

--- 03WordSearch/WordSearch.java
+++ 03WordSearch/WordSearch.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.io.*;
 
 public class WordSearch{
 
@@ -14,24 +13,10 @@ public class WordSearch{
 	    g = new WordGrid(20,20,Integer.parseInt(arg[0]),0);
 	}else{
 	    System.out.println("default");
-	    g = new WordGrid(20,20,1,0);
-	}
-	try{
-	    File words = new File("words.txt");
-	    Scanner infile = new Scanner(words);
-	    ArrayList<String> bank = new ArrayList<String>();
-	    Random r = new Random();
-	    while(infile.hasNext()){
-		bank.add(infile.next());
-	    }
-	    int size = bank.size();
-	    g.addWordList(bank);
-	    System.out.println(g.toString());
-	}
-	catch(FileNotFoundException e){
-	    System.out.println("lol what file");
+	    g = new WordGrid(15,30,1,0);
 	}
 
+	g.loadWordsFromFile("words.txt",false);
     }
 
 }
