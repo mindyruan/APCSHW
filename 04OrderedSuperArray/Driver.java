f417e3f2f42a18fce25451881b0c053b802fe125
Mindy Ruan <mindyruan@users.noreply.github.com>
2014-12-07 21:41:24 -0500

    testing insertionSort()

--- 04OrderedSuperArray/Driver.java
+++ 04OrderedSuperArray/Driver.java
@@ -1,20 +1,30 @@
 public class Driver{
     public static void main(String[]args){
 
+	SuperArray a = new SuperArray(23);
+	for (int i = 0; i < 23; i++){
+	    a.add("" + i);
+	}
+	print(a);
+	print("----------------");
+	a.insertionSort();
+	print(a);
+
+	/*
         OrderedSuperArray b = new OrderedSuperArray(5);
 	System.out.println(b);
 
 	b.add(0,"wowzers");
 	b.add(0,"rad");
 	b.add("coolio");
-	print(b);
 	b.add("lol");
-	print(b);
         b.add(5,"suave");
 	b.add("neato");
 	b.add("terrific");
-	System.out.println(b + "" + b.size());
-	
+	print(b);
+	b.set(0,"magnifico");
+        print(b);
+	*/
     }
 
     public static void print(Object x){
