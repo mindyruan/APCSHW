90cb93ca9a88608879ef22070a059370a8efeedd
Mindy Ruan <mindyruan@users.noreply.github.com>
2014-12-07 22:10:55 -0500

    updated terminal input

--- 04OrderedSuperArray/Driver.java
+++ 04OrderedSuperArray/Driver.java
@@ -1,30 +1,28 @@
 public class Driver{
     public static void main(String[]args){
 
-	SuperArray a = new SuperArray(23);
-	for (int i = 0; i < 23; i++){
+	SuperArray a = new SuperArray(31);
+	for (int i = 0; i < 31; i++){
 	    a.add("" + i);
 	}
-	print(a);
-	print("----------------");
-	a.insertionSort();
+
 	print(a);
 
-	/*
-        OrderedSuperArray b = new OrderedSuperArray(5);
-	System.out.println(b);
+	if (args.length == 1){
+	    if (args[0].equals("array")){
+		a.sort();
+		print(a);
+	    }else if (args[0].equals("insert")){
+		a.insertionSort();
+		print(a);
+	    }else if (args[0].equals("select")){
+		a.selectionSort();
+		print(a);
+	    }
+	}else{
+	    print("please enter java Driver <array/insert/select>");
+	}
 
-	b.add(0,"wowzers");
-	b.add(0,"rad");
-	b.add("coolio");
-	b.add("lol");
-        b.add(5,"suave");
-	b.add("neato");
-	b.add("terrific");
-	print(b);
-	b.set(0,"magnifico");
-        print(b);
-	*/
     }
 
     public static void print(Object x){
